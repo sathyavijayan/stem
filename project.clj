a1fb2fed1ea4ae405dd4cbe8991b99dce84c0cf9
Olivier Bohrer <olivier.bohrer@thetrainline.com>
2017-11-27 11:02:46 +0000

    put test deps in dev profile

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/sathyavijayan/stem"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [midje "1.9.0-alpha6"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-midje "3.2.1"]]
+                   :dependencies [[midje "1.9.0-alpha9"]]}})
