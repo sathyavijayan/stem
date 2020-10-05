ddbfb8c781f8dfcc73f39ca0d6ed0df49a608e5b
Sathyavijayan Vittal <sathyavijayan@gmail.com>
2020-10-05 15:38:44 +0100

    fix broken test

--- test/stem/deprecated/core_test.clj
+++ test/stem/deprecated/core_test.clj
@@ -1,6 +1,6 @@
-(ns stem.core-test
+(ns stem.deprecated.core-test
   (:require [midje.sweet :refer :all]
-            [stem.core :refer :all]
+            [stem.deprecated.core :refer :all]
             [clojure.string :as str])
   (:import [java.time LocalDate]))
 
@@ -21,15 +21,15 @@
 
 
 (tabular
- (fact "render works properly"
-       (render-string (merge DEFAULT-BINDINGS ?addl-bindings) ?template) => ?outcome)
- ?template                           ?addl-bindings                                       ?outcome
- "template with no vars or exprs"    nil                                                  "template with no vars or exprs"
- "${hello}"                          nil                                                  "hello"
- "${hello} ${name}!"                 nil                                                  "hello sathya!"
- "${hello} world ${name}!"           nil                                                  "hello world sathya!"
- "%{(quotation)}"                    nil                                                  (throws Exception)
- "%{(quotation)}"                   {'quotation stem.core-test/quotation}                 (quotation)
- "%{#=(java.lang.System/exit 42)}"   nil                                                  (throws Exception)
- "%{(capitalize \"${name}\")}"      {'capitalize clojure.string/capitalize}               "Sathya"
- )
+  (fact "render works properly"
+    (render-string (merge DEFAULT-BINDINGS ?addl-bindings) ?template) => ?outcome)
+  ?template                           ?addl-bindings                                       ?outcome
+  "template with no vars or exprs"    nil                                                  "template with no vars or exprs"
+  "${hello}"                          nil                                                  "hello"
+  "${hello} ${name}!"                 nil                                                  "hello sathya!"
+  "${hello} world ${name}!"           nil                                                  "hello world sathya!"
+  "%{(quotation)}"                    nil                                                  (throws Exception)
+  "%{(quotation)}"                   {'quotation stem.deprecated.core-test/quotation}                 (quotation)
+  "%{#=(java.lang.System/exit 42)}"   nil                                                  (throws Exception)
+  "%{(capitalize \"${name}\")}"      {'capitalize clojure.string/capitalize}               "Sathya"
+  )
